17ffceb97095c231de7dd1516bbed804a424a9af
glz <gonglz@zhonghongcarav.com.cn>
2016-10-24 10:35:43 +0800

    添加重载函数

--- day02/Test.cpp
+++ day02/Test.cpp
@@ -18,6 +18,13 @@ void Test::SetValue(int val)
 	Value = val;
 	cout << "SetValue=" << Value << endl;
 }
+void Test::SetValue(int val,int val2)
+{
+	Value = val;
+	Value2 = val2;
+	cout << "SetValue=" << Value << endl;
+	cout << "SetValue2=" << Value2 << endl;
+}
 int Test::GetValue(void) const
 {
 	cout << "GetValue=" << Value << endl;
@@ -39,7 +46,9 @@ int main()
 	cout << "please input int Value" << endl;
 	cin >> input;
 	TestClass.SetValue(input);
+	TestClass.SetValue(input,input*2);
 	cout << "Value=" << TestClass.GetValue() << endl;
+	cout << "Value2=" << TestClass.GetValue2() << endl;
 	cout << "Squarse=" << TestClass.GetValueSquare() << endl;
 	return 0;
 }
--- day02/Test.h
+++ day02/Test.h
@@ -7,11 +7,18 @@ public:
 	Test();
 	~Test();
 	void SetValue(int val);
+	void SetValue(int val,int val2);
 	int GetValue(void) const;
 	
 	inline int GetValueSquare(void) const;
 	
+	int GetValue2(void) const
+	{
+		return Value2;
+	}
+	
 private:
 	int Value;
+	int Value2;
 };
 #endif
